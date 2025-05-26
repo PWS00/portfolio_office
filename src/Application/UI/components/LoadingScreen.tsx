d85ca9f30290f88f82ddfbbf78e273c8cba59cdc
Pietro Walter Soriano <79905658+PWS00@users.noreply.github.com>
2025-05-26 18:28:39 +0200

    Update LoadingScreen.tsx

--- src/Application/UI/components/LoadingScreen.tsx
+++ src/Application/UI/components/LoadingScreen.tsx
@@ -149,10 +149,10 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                         <div style={styles.logoContainer}>
                             <div>
                                 <p style={styles.green}>
-                                    <b>Walter Soriano</b>{' '}
+                                    <b>Walter Soriano,</b>{' '}
                                 </p>
                                 <p style={styles.green}>
-                                    <b><WS Inc.,</b>
+                                    <b><WS Inc.</b>
                                 </p>
                             </div>
                         </div>
